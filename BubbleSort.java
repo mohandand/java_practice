707bf8bab0a072ab5ee803fc60aedb0bce0e59e2
mohandand <mohandand@gmail.com>
2021-09-04 21:40:54 -0700

    Added bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class BubbleSort {
+	public static void main(String []args) {
+		int n, i, j, swap;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Plese Enter number of integers to sort:");
+		n = in.nextInt();
+		int array[] = new int[n];
+		System.out.println("Please Enter " + n + " integers");
+		for (i = 0; i < n; i++) 
+		      array[i] = in.nextInt();
+		for (i = 0; i < ( n - 1 ); i++) {
+			for (j = 0; j < n - i - 1; j++) {
+				if (array[j] > array[j+1]) 
+				/* Swapping */ {
+					swap       = array[j];
+					array[j]   = array[j+1];
+					array[j+1] = swap;
+				}
+			}
+		}
+		System.out.println("After Sorting:");
+		for (i = 0; i < n; i++) 
+		      System.out.println(array[i]);
+	}
+}
