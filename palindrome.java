c724485ea0148bc9d8dc86cac136e64193230bc2
mohandand <mohandand@gmail.com>
2021-10-02 11:53:12 -0700

    Added palindrome program

---
+++ palindrome.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+class palindrome
+{
+ 
+// recursive function that returns the reverse of digits
+static int reverse(int input, int temp)
+{
+    if (input == 0)
+        return temp;
+ 
+    // reverse of a number
+    temp = (temp * 10) + (input % 10);
+ 
+    return reverse(input / 10, temp);
+}
+ public static void main (String[] args)
+{
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number to check palindrome or not ..? ");
+    int input = scanner.nextInt();
+    int temp = reverse(input, 0);
+     
+    if (temp == input)
+        System.out.println("yes");
+    else
+        System.out.println("no" );
+}
+}
